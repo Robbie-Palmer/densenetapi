8c4b6634cfffeee8ef0f1ae7224dfdb07e7915b4
Robbie-Palmer <8760191+Robbie-Palmer@users.noreply.github.com>
2021-12-02 21:27:24 +0000

    Accept base64 encoded string into predict function and apply DenseNet model and return prediction Add test for dog image

--- densenetapi/__init__.py
+++ densenetapi/__init__.py
@@ -1,12 +1,30 @@
+import requests
+import torch
+from PIL import Image
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
+from torchvision import transforms
+from io import BytesIO
+import base64
 
 
 class ImagePayload(BaseModel):
     image: str
 
 
+imagenet_class_response = requests.get('https://raw.githubusercontent.com/pytorch/hub/master/imagenet_classes.txt')
+assert imagenet_class_response.status_code == 200
+classes = imagenet_class_response.text.split('\n')
+preprocess = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+model = torch.hub.load('pytorch/vision:v0.10.0', 'densenet121', pretrained=True)
+if torch.cuda.is_available():
+    model.to('cuda')
 app = FastAPI()
 
 
@@ -15,16 +33,26 @@ def home():
     return HTMLResponse(
         """<h1>Welcome to the DenseNet API!</h1>
         <p>To use this service, send a  POST HTTP request to {this-url}/predict</p>
-        <p>The JSON payload has the following format: {"image": "IMAGE_DATA_FOR_DENSENET_TO_CLASSIFY"}</p>
+        <p>The JSON payload has the following format: {"image": "BASE64_STRING_OF_IMAGE"}</p>
         <p>Example request-response pair:</p>
-        Send request to model with curl -X POST -d '{"image": "CAT_IMAGE_DATA"} http://localhost:5000/predict'</p>
+        <p>Send request to model with curl -X POST -d '{"image": "CAT_IMAGE_ENCODED_DATA"}
+        http://localhost:5000/predict'</p>
         This will respond with {"response": "cat"}</p>
         """)
 
 
 @app.post('/predict/')
 def predict(payload: ImagePayload):
-    return payload
+    image = Image.open(BytesIO(base64.b64decode(payload.image)))
+    input_tensor = preprocess(image)
+    input_batch = input_tensor.unsqueeze(0)
+    if torch.cuda.is_available():
+        input_batch = input_batch.to('cuda')
+    with torch.no_grad():
+        output = model(input_batch)
+    probabilities = torch.nn.functional.softmax(output[0], dim=0)
+    top_prob, top_id = torch.topk(probabilities, 1)
+    return classes[top_id]
 
 
 if __name__ == '__main__':
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,27 @@
+import urllib
+
+from PIL import Image
 from fastapi.testclient import TestClient
+from pytest import fixture
 
 from densenetapi import app
+from io import BytesIO
+import base64
+
+
+@fixture
+def encoded_image_and_class():
+    url, filename = ("https://github.com/pytorch/hub/raw/master/images/dog.jpg", "dog.jpg")
+    try:
+        urllib.URLopener().retrieve(url, filename)
+    except:
+        urllib.request.urlretrieve(url, filename)
+    image = Image.open(filename)
+    buffer = BytesIO()
+    image.save(buffer, format="JPEG")
+    bytes = buffer.getvalue()
+    return base64.b64encode(bytes).decode('utf-8'), 'dog'
+
 
 client = TestClient(app)
 
@@ -11,8 +32,9 @@ def test_home():
     assert 'Welcome to the DenseNet API!' in response.text
 
 
-def test_predict():
-    payload = dict(image='test_image')
+def test_predict(encoded_image_and_class):
+    encoded_image, expected_class = encoded_image_and_class
+    payload = dict(image=encoded_image)
     response = client.post('/predict/', json=payload)
     assert response.status_code == 200
-    assert response.json() == payload
+    assert response.json() == expected_class
